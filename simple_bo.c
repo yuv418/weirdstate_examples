7e464a68de9e80b78e746dec24927666a40776ad
cdknight <me@ramesh.tv>
2024-10-02 00:21:01 -0400

    Update examples

--- simple_bo.c
+++ simple_bo.c
@@ -1,30 +1,21 @@
-#include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdint.h>
 #include <errno.h>
 
-#define LEN 5
-
-void copy(uint64_t* data, uint64_t val, uint64_t number) {
+uint64_t* copy(uint64_t val, uint64_t number) {
+  uint64_t* data = malloc(number * sizeof(uint32_t));
   for (uint64_t i = 0; i < number; i++) {
     data[i] = val;
   }
+  return data;
 }
-
 int main(int argc, char **argv) { 
-    // first argument is data to fill, second argument is number of 
-    //  elements
     if (argc != 3) { return 1; }
-
     uint64_t val = strtoul(argv[1], NULL, 0);
     if (errno == ERANGE) { return 1; }
-
     uint64_t number = strtoul(argv[2], NULL, 0);
     if (errno == ERANGE) { return 1; }
-
-    uint64_t* data = malloc(number * sizeof(uint32_t));
-
-    copy(data, val, number);
-
+    uint64_t* data = copy(val, number);
+    free(data);
     return 0;
 }
